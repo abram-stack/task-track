bd0a01bbfedfb5df435246ae410cf0294495491f
abram-stack <anthonybramantyo@outlook.com>
2021-12-15 18:33:56 +0100

    add comp. Task with top level state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Header from './components/Header'
+import Task from './components/Task'
+import { useState } from 'react';
 
 function App() {
-
+  // we want to have the state in top level component 
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    text: 'Appointment with doctor',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: 'Working at Cafe',
+    day: 'Feb 6th at 2:00pm',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: 'Buy Christmas Presents',
+    day: 'Feb 7th at 2:30pm',
+    reminder: false
+    }])
+  
   return (
     <div className="container">
-      <Header/>
+      <Header />
+      <Task tasks={tasks}/>
     </div>
   );
 }
