7361ac1451e640965d7119d99fc261eaf2365fa1
abram-stack <anthonybramantyo@outlook.com>
2021-12-16 00:18:35 +0100

    add remove task (only ui), bug onDoubleClick

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,12 @@
-const Task = ({ task}) => {
+// import styled from 'styled-components'
+// import { Cross } from '@styled-icons/entypo/Cross'
+import CloseIcon from '@mui/icons-material/Close';
+
+const Task = ({ task, onDelete, onToggle}) => {
   return (
-    <div className="task">
-      <h3>{task.text}</h3>
-      <p>{ task.day}</p>
+    <div className="task" onDoubleClick={()=> onToggle(task.id)}>
+      <h3>{task.text}<CloseIcon onClick={() => onDelete(task.id) }/> </h3>
+      <p>{task.day}</p>
     </div>
   )
 }
