f9b40ebb00a7ceea8c05128e75c66b242d8b0486
abram-stack <anthonybramantyo@outlook.com>
2021-12-16 11:54:03 +0100

    create addTask form

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,22 @@
 // import styled from 'styled-components'
 // import { Cross } from '@styled-icons/entypo/Cross'
-import CloseIcon from '@mui/icons-material/Close';
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import CloseIcon from "@mui/icons-material/Close";
 
-const Task = ({ task, onDelete, onToggle}) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task" onDoubleClick={()=> onToggle(task.id)}>
-      <h3>{task.text}<CloseIcon onClick={() => onDelete(task.id) }/> </h3>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`}>
+      <h3>
+        {task.text}
+        <CloseIcon onClick={() => onDelete(task.id)} />
+      </h3>
       <p>{task.day}</p>
+      <h3>
+        Remind me
+        <AccessAlarmIcon onDoubleClick={() => onToggle(task.id)} />
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
