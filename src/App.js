da9a5d327b07213bb50e45b22cda24d3963cb2d6
abram-stack <anthonybramantyo@outlook.com>
2021-12-16 12:25:56 +0100

    comp level state onSubmit input form

--- src/App.js
+++ src/App.js
@@ -24,6 +24,11 @@ function App() {
     reminder: false
     }])
   
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+  }
+
   // Delete task 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,7 +44,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) :
         ('No to show')
       }
