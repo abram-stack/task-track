c9ccc27540e209e3fc07ad7ccde9112b4fcf85aa
abram-stack <anthonybramantyo@outlook.com>
2021-12-17 14:20:39 +0100

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,86 +1,111 @@
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask';
-import { useState, useEffect } from 'react';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  // we want to have the state in top level component 
+  // we want to have the state in top level component
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([])
-  
+  const [tasks, setTasks] = useState([]);
+
   // useEffect is async function
   useEffect(() => {
     const getTasks = async () => {
       const fetchFromDB = await fetchTasks();
       setTasks(fetchFromDB);
-    }
+    };
     getTasks();
-  }, [])
+  }, []);
 
   // FETCH DATA
-  const fetchTasks = async() => {
-      const result = await fetch('http://localhost:5000/tasks');
-      const data = await result.json();
-      return data;
-  }
+  const fetchTasks = async () => {
+    const result = await fetch("http://localhost:5000/tasks");
+    const data = await result.json();
+    return data;
+  };
 
-  const fetchTask = async(id) => {
-      const result = await fetch(`http://localhost:5000/tasks/${id}`);
-      const data = await result.json();
-      return data;
-  }
+  const fetchTask = async (id) => {
+    const result = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await result.json();
+    return data;
+  };
 
   // ADDTASK
   const addTask = async (task) => {
     // post to server
-    const result = await fetch('http://localhost:5000/tasks', {
-      method: 'POST',
+    const result = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json'
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(task)
-    })
+      body: JSON.stringify(task),
+    });
     // set state UI
     const data = await result.json();
     setTasks([...tasks, data]);
-  }
+  };
 
-
-  // Delete task 
-  const deleteTask = async(id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' });
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+  // Delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   // TOGGLE REMINDER
   const toggleReminder = async (id) => {
     // get single task from server
     const taskToToggle = await fetchTask(id);
-    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder};
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
     const result = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers:{
-        'Content-type': 'application/json'
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(updTask)
-    })
+      body: JSON.stringify(updTask),
+    });
     //get the result as js object
     const data = await result.json();
 
     // change state UI
-    setTasks(tasks.map((task) =>  
-      task.id == id ? { ...task, reminder: data.reminder } :
-        task
-    ))
-  }
+    setTasks(
+      tasks.map((task) =>
+        task.id == id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      { showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) :
-        ('No to show')
-      }
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                "No to show"
+              )}
+            </>
+          )}
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from "prop-types";
+import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'close' : 'add' } onClick={onAdd}/>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "close" : "add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: 'Task Tracker'
-}
+  title: "Task Tracker",
+};
 Header.propTypes = {
-  title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
 
 // // CSS in js
 // const headingStyle = {
 //   color: 'red',
 //   backgroundColor: 'black'
 // }
-export default Header
+export default Header;
